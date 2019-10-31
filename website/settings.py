1979e5c6f2e93058271c6ef9b06a67ce0f389683
Borja Sotomayor <borja@cs.uchicago.edu>
2019-10-31 17:57:47 -0500

    Fixed allowed hosts

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Please set SECRET_KEY environment variable
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "cs121-webapp.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "cs121-officehours.herokuapp.com"]
 
 
 # Application definition
