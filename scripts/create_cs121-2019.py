3d8f95d0bf53c2b1f4102d9e9fe2114421c195f4
Borja Sotomayor <borja@cs.uchicago.edu>
2019-11-03 13:50:31 -0600

    Added script for creating initial CS 121 objects

---
+++ scripts/create_cs121-2019.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import os, django, sys
+
+sys.path.append("../")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+django.setup()
+
+
+import webapp.models as models
+
+aut19, created = models.Quarter.objects.get_or_create(ais_num = 2198,
+                                                      year = 2019,
+                                                      quarter = "aut")
+
+
+cs121, created = models.Course.objects.get_or_create(ais_num = 56311,
+                                                     subject = "CMSC",
+                                                     catalog_code = 12100,
+                                                     name = "Computer Science with Applications 1")
+
+cs121_aut_19, created = models.CourseOffering.objects.get_or_create(course = cs121,
+                                                                    quarter = aut19,
+                                                                    url_slug = "cmsc12100-aut-19")
