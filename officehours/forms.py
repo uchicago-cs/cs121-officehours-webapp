500e82e401c602b09e4e1aea8a227bd445b66fc1
Borja Sotomayor <borja@cs.uchicago.edu>
2020-10-05 13:22:31 -0500

    [GH-19] Add basic Zoom URL validation

--- officehours/forms.py
+++ officehours/forms.py
@@ -23,3 +23,11 @@ class RequestForm(forms.ModelForm):
             'zoom_url': _('URL of your Zoom meeting:'),
             'description': _('Describe the issue you need help with at office hours:')
         }
+
+    def clean_zoom_url(self):
+        zoom_url = self.cleaned_data['zoom_url']
+
+        if not zoom_url.startswith("https://uchicago.zoom.us/"):
+            raise ValidationError("Please enter a valid Zoom URL in the uchicago.zoom.us domain")
+
+        return zoom_url
