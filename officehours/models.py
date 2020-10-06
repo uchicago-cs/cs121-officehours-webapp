39088b7bdf06875b8b924add8e63b9911dcfceaa
Borja Sotomayor <borja@cs.uchicago.edu>
2020-10-06 13:59:00 -0500

    Update the server when starting service, in case it's not the same as the one who scheduled it

--- officehours/models.py
+++ officehours/models.py
@@ -397,6 +397,7 @@ class Request(models.Model):
 
     def start_service(self, server):
         self.state = Request.STATE_INPROGRESS
+        self.server = server
         self.service_start_time = timezone.localtime(timezone.now())
         self.save()
 
